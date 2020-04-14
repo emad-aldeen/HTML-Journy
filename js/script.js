be4f874f6ff388c99363376a934451f77e880b79
3madov_7 <emadzxy7@gmail.com>
2020-04-14 21:36:27 +0300

    add new stuf, bug fixed

---
+++ js/script.js
@@ -0,0 +1,62 @@
+'use strict'
+var today = new Date();
+var hourNow = today.getHours();
+if (hourNow >= 20) {
+    var r = confirm("Its late now go sleep");
+    if (r == true) {
+        window.location = "sleep.html";
+    } else {
+        var z = confirm("So .. Are you Instructer or TA?")
+        if (z == true) {
+            var y = prompt("Whats your Name?")
+            if (y == "samer" || y == "saad") {
+                alert('Welcome ' + y ' glad to see you agin .. :)');
+            } else {
+                window.location = "sleep.html";
+            }
+        } else {
+            window.location = "sleep.html";
+        }
+    }
+} else {
+    alert("this weebsite not for -17 !!");
+    var age = prompt('So what is your age?');
+
+    if (age <= 17) {
+        window.location = "byby.html";
+    } else {
+        alert("are you Sure?")
+
+        alert(" im seeing you now O_O ")
+    }
+
+}
+
+
+function newfunc() {
+    var x = document.getElementById("cheak");
+
+    if (x.checked == true) {
+        document.body.style.background = 'black';
+    } else {
+        document.body.style.background = '';
+    }
+};
+function newfunc1() {
+    var x = document.getElementById("cheak1");
+
+    if (x.checked == true) {
+        document.body.style.background = 'blue';
+    } else {
+        document.body.style.background = '';
+    }
+};
+function newfunc2() {
+    var x = document.getElementById("cheak2");
+
+    if (x.checked == true) {
+        document.body.style.background = 'white';
+    } else {
+        document.body.style.background = '';
+    }
+};
---
+++ js/script2.js
@@ -0,0 +1,8 @@
+/*alert("hmmm  O_O");*/
+var y = confirm("Are you sure?")
+
+if (y == true) {
+    alert("you are the response of your choise");
+} else {
+    window.location = "noob.html";
+};
